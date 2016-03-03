060faf254db29528e9f7dabeb0998c40b68db696
Ruebel Holmes <treyholmes@yahoo.com>
2016-03-03 14:23:55 -0600

    Comments (beginning of code)

---
+++ Blackjack.app/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Blackjack!");
+            Console.WriteLine("House Rules:");
+            Console.WriteLine("Dealer stands on 16 or higher");
+            Console.WriteLine("Aces are 11 points each (double aces = 22).");
+            Console.ReadLine();
+
+            //Welcome screen (CHECK)
+            //display the game
+            //52 cards
+            //4 suits
+            //card #'s
+            //card values
+            //points
+            //new shuffled deck everytime
+            //deal
+            //show 1 dealer card
+            //Private class dealer?
+            //show all player cards
+            //first flop 21 = blackjack
+            //stay/ hit
+            //busts
+            //build a class for all suits
+            //build a class for all cards
+            //loops for play
+            //2-9 values 2-9
+            //10, jack, queen, king values @10
+            //Ace valued at 11
+            //If/then for hands (hit, stay)
+            //dealer score, and player score 
+            //Dealer stays on 16 or higher, draws if lower
+            //if dealer >= 16-21 then stay
+            //dealer < 16 draw until 16 or higher (A loop)
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            /*public class card
+            {
+                public string ID {get; set;)
+                public string Suit {get; set}
+                public string
+
+    */
+        }
+    }
+}
